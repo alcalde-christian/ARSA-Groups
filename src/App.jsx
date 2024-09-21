eb50509eb90dce74801651592844a64099c0ade7
Christian Alcalde <alcalde_christian@hotmail.com>
2024-09-21 00:47:02 -0300

    analytics config testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header from "./components/Header/Header"
 import NameAdder from "./components/NameAdder/NameAdder"
 import Footer from "./components/Footer/Footer"
+import ReactGA from "react-ga"
+import { useEffect } from "react"
 import styles from "./App.module.css"
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-HR5EB1RMQQ")
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <div className={styles.wrapper}>
       <Header />
